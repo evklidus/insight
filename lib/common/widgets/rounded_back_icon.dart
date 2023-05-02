4f5527e675ce245ac7e0a3763e232600cf19bf0b
erik.minasov <erik.minasov@rt.ru>
2023-05-02 22:04:01 +0300

    Refactored rounded button code

--- lib/common/widgets/rounded_back_icon.dart
+++ lib/common/widgets/rounded_back_icon.dart
@@ -11,31 +11,22 @@ class RoundedBackIcon extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () => onTap?.call() ?? context.popRoute(),
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(20.r),
-        child: Container(
-          height: 40.w,
-          width: 40.w,
-          decoration: BoxDecoration(
-            color: Theme.of(context).colorScheme.surface,
-            shape: BoxShape.circle,
-          ),
-          padding: EdgeInsets.symmetric(
-            vertical: 8.w,
-            horizontal: 5.h,
-          ),
-          child: SizedBox(
-            height: 24,
-            width: 18,
-            child: Padding(
-              padding: EdgeInsets.only(right: 1.w),
-              child: const FittedBox(
-                fit: BoxFit.cover,
-                child: Icon(
-                  Icons.arrow_back_ios_new_rounded,
-                ),
-              ),
-            ),
+      child: Container(
+        height: 40.w,
+        width: 40.w,
+        margin: EdgeInsets.only(right: 1.w),
+        padding: EdgeInsets.symmetric(
+          vertical: 8.w,
+          horizontal: 5.h,
+        ),
+        decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.surface,
+          shape: BoxShape.circle,
+        ),
+        child: const FittedBox(
+          fit: BoxFit.cover,
+          child: Icon(
+            Icons.arrow_back_ios_new_rounded,
           ),
         ),
       ),
