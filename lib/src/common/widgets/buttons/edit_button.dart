69d8346d579592ec8e4845221ef65229769e7088
evklidus <minasoverik@gmail.com>
2024-07-13 16:10:12 +0300

    Поправил EditButton (неверно отображался на релизе)

--- lib/src/common/widgets/buttons/edit_button.dart
+++ lib/src/common/widgets/buttons/edit_button.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:insight/src/common/constants/base_constants.dart';
-import 'package:insight/src/common/utils/extensions/context_extension.dart';
 import 'package:insight/src/common/widgets/buttons/adaptive_button.dart';
 
 /// {@template edit_button}
@@ -23,22 +22,11 @@ class EditButton extends StatelessWidget {
   Widget build(BuildContext context) => AnimatedOpacity(
         duration: standartDuration,
         opacity: opacity,
-        child: Padding(
-          padding: const EdgeInsets.only(bottom: 4),
-          child: SizedBox.fromSize(
-            size: const Size(100, 48),
-            child: AdaptiveButton(
-              onPressed: onPressed,
-              padding: EdgeInsets.zero,
-              color: context.colorScheme.surfaceContainerHigh,
-              borderRadius: BorderRadius.circular(24),
-              child: Expanded(
-                child: Text(
-                  isEditing ? 'Сохранить' : 'Изменить',
-                  style: context.textTheme.bodyMedium,
-                ),
-              ),
-            ),
+        child: AdaptiveButton(
+          onPressed: onPressed,
+          padding: EdgeInsets.zero,
+          child: Text(
+            isEditing ? 'Сохранить' : 'Изменить',
           ),
         ),
       );
