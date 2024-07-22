80436877397749857e70e190e94815b763333670
evklidus <minasoverik@gmail.com>
2024-07-23 02:28:54 +0300

    Улучшил TestValues виджет

--- lib/src/common/widgets/test_values_widget.dart
+++ lib/src/common/widgets/test_values_widget.dart
@@ -1,14 +1,21 @@
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:insight/gen/pubspec.yaml.g.dart';
 import 'package:insight/src/common/utils/build_mode.dart';
 import 'package:insight/src/common/utils/current_flavor.dart';
 import 'package:insight/src/common/utils/extensions/context_extension.dart';
+import 'package:insight/src/features/profile/bloc/profile_bloc.dart';
+import 'package:insight_snackbar/insight_snackbar.dart';
+import 'package:provider/provider.dart';
 
 class TestValues extends StatelessWidget {
   const TestValues({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final profileBloc = Provider.of<ProfileBloc>(context);
+    final userId = profileBloc.state.data?.id ?? 'ID отсутствует';
+
     return Container(
       padding: const EdgeInsets.all(16),
       decoration: ShapeDecoration(
@@ -24,6 +31,11 @@ class TestValues extends StatelessWidget {
           _TestValueText(
             title: 'version',
             value: Pubspec.version.representation,
+          ),
+          const SizedBox(height: 8),
+          _TestValueText(
+            title: 'User ID',
+            value: userId,
           ),
         ],
       ),
@@ -42,18 +54,32 @@ class _TestValueText extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return RichText(
-      text: TextSpan(
-        text: '$title: ',
-        children: <TextSpan>[
-          TextSpan(
-            text: value,
-            style: DefaultTextStyle.of(context).style.copyWith(
-                  color: context.colorScheme.primary,
-                  fontWeight: FontWeight.bold,
-                ),
+    return GestureDetector(
+      onTap: () {
+        Clipboard.setData(ClipboardData(text: value));
+        InsightSnackBar.showInfo(
+          context,
+          text: 'Значение скопировано',
+        );
+        HapticFeedback.selectionClick();
+      },
+      child: Padding(
+        padding: const EdgeInsets.all(2),
+        child: RichText(
+          maxLines: 5,
+          text: TextSpan(
+            text: '$title: ',
+            children: <TextSpan>[
+              TextSpan(
+                text: value,
+                style: DefaultTextStyle.of(context).style.copyWith(
+                      color: context.colorScheme.primary,
+                      fontWeight: FontWeight.bold,
+                    ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
